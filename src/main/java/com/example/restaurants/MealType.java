338b68a64c10ad281d195ac58db65aa2795094c8
ajajcanin1 <ajajcanin1@etf.unsa.ba>
2019-02-12 21:51:51 +0100

    dodane klase

---
+++ src/main/java/com/example/restaurants/MealType.java
@@ -0,0 +1,45 @@
+package com.example.restaurants;
+
+import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="meal_types")
+public class MealType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="category_id", nullable=false)
+    private long id;
+
+    @Column(name="name", nullable=false, unique=true)
+    private String name;
+
+    @OneToMany(mappedBy = "mealType")
+    private List<Meal> meals;
+
+    public MealType(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Categories{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/restaurants/Table.java
@@ -0,0 +1,48 @@
+package com.example.restaurants;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@javax.persistence.Table(name="tables")
+public class Table {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="table_id", nullable=false)
+    private long id;
+
+    @Column(name="capacity", nullable=false)
+    private int capacity;
+
+    @ManyToOne
+    @JoinColumn(name="restaurant_id")
+    private Restaurant restaurant;
+
+    @OneToMany(mappedBy = "table")
+    private List<Reservation> reservations;
+
+    public Table(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "Table{" +
+                "id=" + id +
+                ", capacity=" + capacity +
+                '}';
+    }
+}
