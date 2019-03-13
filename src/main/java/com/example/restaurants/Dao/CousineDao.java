56f64bf1aef10513255fa87b51ebaa9c2a250f32
ajajcanin1 <ajajcanin1@etf.unsa.ba>
2019-03-13 14:12:33 +0100

    fixed CousineDao

--- src/main/java/com/example/restaurants/Dao/CousineDao.java
+++ src/main/java/com/example/restaurants/Dao/CousineDao.java
@@ -1,7 +1,9 @@
 package com.example.restaurants.Dao;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CousineDao {
     ResponseEntity getAllCousines();
 }
