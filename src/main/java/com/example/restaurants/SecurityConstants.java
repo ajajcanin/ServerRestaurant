ffa6384b75c7efa77e32ebcee8d4bf70d6dea28b
ajajcanin1 <ajajcanin1@etf.unsa.ba>
2019-03-13 13:38:49 +0100

    added security constant for getting cousines

--- src/main/java/com/example/restaurants/SecurityConstants.java
+++ src/main/java/com/example/restaurants/SecurityConstants.java
@@ -13,5 +13,6 @@ public class SecurityConstants {
     public static final String CITIES = "/app/cities";
     public static final String SEARCH = "/app/search";
     public static final String RESTAURANTS_BY_FILTER = "/app/getRestaurantsByFilter";
+    public static final String COUSINES = "/app/cousines";
 
 }
