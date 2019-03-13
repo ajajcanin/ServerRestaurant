00d5f3e74ba08888dfbd1f63409a0d624e746b54
ajajcanin1 <ajajcanin1@etf.unsa.ba>
2019-03-13 15:07:22 +0100

    fixed Cousine Controller

--- src/main/java/com/example/restaurants/Dao/CousineDao.java
+++ src/main/java/com/example/restaurants/Dao/CousineDao.java
@@ -1,7 +1,6 @@
 package com.example.restaurants.Dao;
 
 import com.example.restaurants.Entity.Cousine;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
